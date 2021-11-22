17fa35417c38f9181310743aa8d6b4043c9e2711
salvosanzone <salvosansone.ss@gmail.com>
2021-11-22 17:20:35 +0100

    modifica del css

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const app = new Vue({
 
   data: {
 
-    email: '',
     //di defaul non vedo l'email
     isLoading: true,
     //in caso di errore
@@ -34,10 +33,10 @@ const app = new Vue({
 
      .then((response) =>{
       //tutta la risposta
-      //console.log('response',response);
+      console.log('response',response);
 
       // il JSON -> quello che a noi interessa
-      //console.log('response.data',response.data);
+      console.log('response.data',response.data);
 
       //salvo,per comodità, in una variabile l'oggetto che a noi interessa
       const data = response.data;
@@ -45,22 +44,20 @@ const app = new Vue({
       //in base a come è fatto ogni response.data prendiamo quello che ci serve
       console.log('email:',data.response);
 
-      //al mio dato vuoto attribuisco una email proveniente dall'oggetto
-      this.email = data.response;
 
       //pusho all'intero dell'array vuoto l'email
-      this.emails.push(this.email) ;
+      this.emails.push(data.response);
 
       //ottenuta l'email verrà mostrata se è true,quindi diversa da se stessa
       this.isLoading = false;
     })
 
+    
     //se sbaglio indirizzo mi indica l'errore
     .catch((error) =>{
       console.log(error);
       this.httpError = true;
     })
-
     }
     
   }
